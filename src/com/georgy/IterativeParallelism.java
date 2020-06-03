c8bdb1ed5fad5f7b5345dce2a78ffce841813b52
Salemar <fireshowel@gmail.com>
2020-06-03 19:41:38 +0300

    initial commit

---
+++ src/com/georgy/IterativeParallelism.java
@@ -0,0 +1,98 @@
+package com.georgy;
+
+import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class IterativeParallelism implements ScalarIP {
+    public IterativeParallelism() {
+
+    }
+
+    public IterativeParallelism(ParallelMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    ParallelMapper mapper;
+
+    @Override
+    public <T> T maximum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
+
+        Function<List<? extends T>, T> function =maxList -> maxList.stream().max(comparator).get();
+        return function.apply(runThreads(threads, values, function));
+    }
+
+    static class workList<T, R> implements Runnable {
+        private final Function<List<? extends T>, R> function;
+        private final List<? extends T> list;
+        private R result;
+
+        public workList(Function<List<? extends T>, R> function, List<? extends T> list) {
+            this.function = function;
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            result = function.apply(list);
+        }
+
+        public R getResult() {
+            return result;
+        }
+    }
+
+    private <T,R> List<R> runThreads(int threads, List<? extends T> values, Function<List<? extends T>, R> function) throws InterruptedException {
+        List<List<? extends T>> jobsList = splitList(threads, values);
+        if (mapper!=null){
+            return mapper.map(function,jobsList);
+        }
+        List<workList<T, R>> workers = jobsList.stream().map(val -> new workList<>(function, val)).collect(Collectors.toList());
+        List<Thread> threadsList = workers.stream().map(Thread::new).collect(Collectors.toList());
+        threadsList.forEach(Thread::start);
+        for (Thread thread : threadsList) {
+            thread.join();
+        }
+
+        return workers.stream().map(workList::getResult).collect(Collectors.toList());
+
+    }
+
+    private <T> List<List<? extends T>> splitList(int threads, List<? extends T> values) {
+        if (threads < 1) {
+            throw new IllegalArgumentException("Wrong threads count: " + threads);
+        }
+
+        int chunksize = threads < values.size() ? values.size() / threads : 1;
+        List<List<? extends T>> result = new ArrayList<>();
+
+        for (int chunkStart = 0; chunkStart < values.size(); chunkStart += chunksize) {
+            int chunkEnd = Math.min(chunkStart + chunksize, values.size());
+            result.add(values.subList(chunkStart, chunkEnd));
+        }
+        return result;
+
+    }
+
+    @Override
+    public <T> T minimum(int threads, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
+        return maximum(threads, values, comparator.reversed());
+    }
+
+    @Override
+    public <T> boolean all(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
+        Function<List<? extends T>, Boolean> function = val -> val.stream().allMatch(predicate);
+        return runThreads(threads,values,function).stream().allMatch(val -> val);
+    }
+
+    @Override
+    public <T> boolean any(int threads, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
+        return !all(threads, values, predicate.negate());
+    }
+}
---
+++ src/com/georgy/ParallelMapperImpl.java
@@ -0,0 +1,124 @@
+package com.georgy;
+
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ParallelMapperImpl implements ParallelMapper {
+    private final Queue<Job<?, ?>> jobQueue = new LinkedList<>();
+    private final List<Thread> threads = new ArrayList<>();
+
+    public ParallelMapperImpl(int threadsNumber) {
+        if (threadsNumber < 1) {
+            throw new IllegalArgumentException("Illegal threads number " + threads);
+        }
+
+        for (int i = 0; i < threadsNumber; ++i) {
+            threads.add(new Thread(new JobExecutor()));
+        }
+        threads.forEach(Thread::start);
+    }
+
+    /**
+     * Maps function {@code function} over specified {@code args}.
+     * Mapping for each element performs in parallel.
+     *
+     * @param function function
+     * @param args     args
+     * @throws InterruptedException if calling thread was interrupted
+     */
+    @Override
+    public <T, R> List<R> map(Function<? super T, ? extends R> function, List<? extends T> args) throws InterruptedException {
+        List<Job<T, R>> jobList = new ArrayList<>();
+        SyncedClock counter = new SyncedClock(args.size());
+        args.forEach(arg -> jobList.add(new Job<>(function, arg, counter)));
+
+        synchronized (jobQueue) {
+            jobQueue.addAll(jobList);
+            jobQueue.notify();
+        }
+
+        counter.waitUntilFinish();
+        return jobList.stream().map(Job::getResult).collect(Collectors.toList());
+    }
+
+    @Override
+    public void close() {
+        threads.forEach(Thread::interrupt);
+        synchronized (jobQueue) {
+            jobQueue.clear();
+        }
+    }
+
+    private static class Job<T, R> {
+        private final Function<? super T, ? extends R> function;
+        private final T argument;
+        private final SyncedClock clock;
+        private R result;
+
+        public Job(Function<? super T, ? extends R> function, T arg, SyncedClock counter) {
+            this.function = function;
+            this.argument = arg;
+            this.clock = counter;
+        }
+
+        public void run() {
+            result = function.apply(argument);
+            clock.incAndNotifyOnFinish();
+        }
+
+        public R getResult() {
+            return result;
+        }
+    }
+
+    private class JobExecutor implements Runnable {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                Job<?, ?> job;
+                synchronized (jobQueue) {
+                    while (jobQueue.isEmpty()) {
+                        try {
+                            jobQueue.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+
+                    job = jobQueue.poll();
+                    if (!jobQueue.isEmpty()) {
+                        jobQueue.notify();
+                    }
+                }
+                job.run();
+            }
+        }
+    }
+}
+
+class SyncedClock {
+    final int resultedValue;
+    volatile int value = 0;
+
+    public SyncedClock(int finishValue) {
+        this.resultedValue = finishValue;
+    }
+
+    public synchronized void incAndNotifyOnFinish() {
+        if (++value == resultedValue) {
+            this.notify();
+        }
+    }
+
+    public synchronized void waitUntilFinish() throws InterruptedException {
+        while (resultedValue != value) {
+            this.wait();
+        }
+    }
+}
