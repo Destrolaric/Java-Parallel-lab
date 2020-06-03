ffdcebbb7cffb01398f587c7509669ddd323460c
Salemar <fireshowel@gmail.com>
2020-06-03 19:46:15 +0300

    added launch script

---
+++ out/production/JavaLastTwoLabs/launch.sh
@@ -0,0 +1,2 @@
+java -cp . -p . -m info.kgeorgiy.java.advanced.concurrent scalar com.georgy.IterativeParallelism
+java -cp . -p . -m info.kgeorgiy.java.advanced.mapper scalar com.georgy.IterativeParallelism
